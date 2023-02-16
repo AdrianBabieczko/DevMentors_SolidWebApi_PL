ccb78eb7b2962961c00d6d135df7731217404497
AdrianBabieczko <adrian.babieczko@gmail.com>
2023-02-16 22:42:27 +0100

    Add Reservations controller with empty actions

---
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySpot.Api.Controllers;
+
+[Route(template:"reservations")]
+public class ReservationsController : ControllerBase
+{
+    [HttpGet]
+    public void Get()
+    {
+    }
+    
+    [HttpPost]
+    public void Post()
+    {
+    }
+}
--- src/MySpot.Api/Models/Reservation.cs
+++ src/MySpot.Api/Models/Reservation.cs
@@ -4,4 +4,6 @@ public class Reservation
 {
     public string EmployeeName { get; set; }
     public string ParkingSpotName { get; set; }
+    public string LicensePlate { get; set; } //Nr rejestracyjny
+    public DateTime Date { get; set; }
 }
