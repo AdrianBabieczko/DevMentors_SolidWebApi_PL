2f9e77bee4185153cc67b10b2e9d602948a9059f
AdrianBabieczko <adrian.babieczko@gmail.com>
2023-02-20 18:14:38 +0100

    Implement CRUD in ReservationsController

--- src/MySpot.Api/Controllers/ReservationsController.cs
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -7,29 +7,80 @@ namespace MySpot.Api.Controllers;
 [Route(template:"reservations")]
 public class ReservationsController : ControllerBase
 {
-    private int _id = 1;
-    private readonly List<Reservation> _reservations = new();
-    private readonly List<string> _parkingSpotNames = new()
+    private static int _id = 1;
+    private static readonly List<Reservation> Reservations = new();
+    private static readonly List<string> ParkingSpotNames = new()
     {
         "P1", "P2", "P3", "P4", "P5"
     };
 
     [HttpGet]
-    public void Get()
+    public ActionResult<IEnumerable<Reservation>> Get() => Ok(Reservations);
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Reservation> Get(int id)
     {
+        var reservation = Reservations.SingleOrDefault(x => x.Id == id);
+        if (reservation is null)
+        { 
+            HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+            return NotFound();
+        }
+
+        return Ok(reservation);
     }
+    
 
     [HttpPost]
-    public void Post(Reservation reservation)
+    public ActionResult Post(Reservation reservation)
     {
-        if (_parkingSpotNames.All(x=>x != reservation.ParkingSpotName))
+        if (ParkingSpotNames.All(x=>x != reservation.ParkingSpotName))
         {
-            return;
+            return BadRequest();
+        }
+
+        reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+        
+        var reservationAlreadyExists = Reservations.Any(x =>
+            x.ParkingSpotName == reservation.ParkingSpotName && x.Date.Date == reservation.Date.Date);
+
+        if (reservationAlreadyExists)
+        {
+            return BadRequest();
         }
         
         reservation.Id = _id;
-        reservation.Date = DateTime.UtcNow.AddDays(1).Date;
         _id++;
-        
+        Reservations.Add(reservation);
+
+        return CreatedAtAction(nameof(Get), new { id = reservation.Id }, null);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Reservation reservation)
+    {
+        var existingReservation = Reservations.SingleOrDefault(x => x.Id == id);
+        if (existingReservation is null)
+        { 
+            HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+            return NotFound();
+        }
+
+        existingReservation.LicensePlate = reservation.LicensePlate;
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var existingReservation = Reservations.SingleOrDefault(x => x.Id == id);
+        if (existingReservation is null)
+        { 
+            HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+            return NotFound();
+        }
+
+        Reservations.Remove(existingReservation);
+        return NoContent();
     }
 }
