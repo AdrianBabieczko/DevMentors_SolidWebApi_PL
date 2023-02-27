4de3abe3dacf23d55c7473dc205dbf6fe976b277
AdrianBabieczko <adrian.babieczko@gmail.com>
2023-02-27 22:48:07 +0100

    Reservation refactor in progres

--- src/MySpot.Api/Entities/Reservation.cs
+++ src/MySpot.Api/Entities/Reservation.cs
@@ -2,9 +2,16 @@ namespace MySpot.Api.Entities;
 
 public class Reservation
 {
-    public int Id { get; set; }
-    public string EmployeeName { get; set; }
-    public string ParkingSpotName { get; set; }
-    public string LicensePlate { get; set; } //Nr rejestracyjny
-    public DateTime Date { get; set; }
+    public int Id { get;}
+    public string EmployeeName { get; private set; }
+    public string LicensePlate { get; private set; } //Nr rejestracyjny
+    public DateTime Date { get; private set; }
+
+    public Reservation(int id, string employeeName, string licensePlate, DateTime date)
+    {
+        Id = id;
+        EmployeeName = employeeName;
+        LicensePlate = licensePlate;
+        Date = date;
+    }
 }
--- src/MySpot.Api/Entities/WeeklyParkingSpot.cs
+++ src/MySpot.Api/Entities/WeeklyParkingSpot.cs
@@ -2,5 +2,11 @@ namespace MySpot.Api.Entities;
 
 public class WeeklyParkingSpot
 {
-    
+    private readonly HashSet<Reservation> _reservations = new();
+
+    public Guid Id { get;}
+    public DateTime From { get; }
+    public DateTime To { get;}
+    public string Name { get; }
+    public IEnumerable<Reservation> Reservations => _reservations;
 }
